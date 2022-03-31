2c3f1686b74c3453265a7ceb572a8c7e35525ee2
Brian Cornelius <brian@Brians-MacBook-Pro.local>
2022-03-30 20:01:57 -0700

    backend hosted on port 4000

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const http = require('http')
 
 const hostname = '127.0.0.1'
-const port = 3000
+const port = 4000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello stupid\n')
+    res.end('Backend running\n')
   })
 
   server.listen(port, hostname, () => {
